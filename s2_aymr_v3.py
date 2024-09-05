c15fa6250d1a376bede6f19f746b73d18b7634db
Alejandro Villegas <jesus.villegas@alumnos.cide.edu>
2024-09-05 17:37:17 -0600

    eee

--- s2_aymr_v3.py
+++ s2_aymr_v3.py
@@ -62,10 +62,10 @@ st.header("Chat")
 st.subheader("Instrucciones")
 
 st.write(""""
-         1. Selecciona el nombre de la o las normas con las que deseas chatear\n\n\n
-         2. Utiliza el espacio de texto realizar una consulta. La consulta puede ser una pregunta, o alguna instrucción, como por ejemplo, detallar un aspecto de las normas seleccionadas. \n\n\n
-         3. Pulsa chatear\n\n\n
-         4. Ingresa las consultas que deseees
+         1. Selecciona el nombre de la o las normas con las que deseas chatear
+         \n\n\n2. Utiliza el espacio de texto realizar una consulta. La consulta puede ser una pregunta, o alguna instrucción, como por ejemplo, detallar un aspecto de las normas seleccionadas.
+         \n\n\n3. Pulsa chatear
+         \n\n\n4. Ingresa las consultas que deseees
          """)
 
         
