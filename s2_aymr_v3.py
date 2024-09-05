15a2dc39fd7aa78f72ed66098a888782e8cc662b
Alejandro Villegas <jesus.villegas@alumnos.cide.edu>
2024-09-05 17:51:10 -0600

    eeee

--- s2_aymr_v3.py
+++ s2_aymr_v3.py
@@ -58,15 +58,44 @@ if st.button ('Clasificar'):
 
 #                               Chat
 
+#Paso 4: Headersm, instucciones y selección de normas del chat
+
 st.header("Chat")
 st.subheader("Instrucciones")
 
-st.write(""""
+st.write("""
          1. Selecciona el nombre de la o las normas con las que deseas chatear
          \n\n\n2. Utiliza el espacio de texto realizar una consulta. La consulta puede ser una pregunta, o alguna instrucción, como por ejemplo, detallar un aspecto de las normas seleccionadas.
          \n\n\n3. Pulsa chatear
          \n\n\n4. Ingresa las consultas que deseees
          """)
 
-        
+normas_chat = st.multiselect("Elije la o las normas con la que deseas chatear chatear: ",['2013-3-0-AC_V317', '2013-40-2-AC_V72', '2014-36-0-AC_V18', '2014-56-1-AC_V80', '2014-56-2-AC_V37', '2018-28-2-AC_V03', '2018-45-1-AC_V12', '2019-9-2-AC_V05', '2020-3-1-AC_V02', '2020-12-0-AC_V07', '2021-0-134-DD_V15', '2023-6-2-AC_V01'])
 
+#-*-*-*-*-*-*-*-*-*-*-*
+#Paso 5: El chat
+
+#Para almacenar los mensajes en una sesion
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+#Funcion para enviar mensajes
+
+def send_message():
+    user_message = st.session_state.user_input
+    if user_message:
+        st.sessin_state.messages.append({"user":user_message, "bot": "Soy Aymr. Si ya seleccionaste las normas sobre las que estas interesado, por favor haz una conulta"})
+        st.session_state.user_input = ""
+
+#Entrada de texto para el usuario
+
+st.text_input ("Tu mensaje", key = "user_input", on_change=send_message)
+
+
+#Mostrar el chat
+
+for message in st.session_state.messages:
+    with st.chat_message("user"):
+        st.markdown(message["user"])
+    with st.chat_message("bot"):
+        st.markdown (message["bot"])
